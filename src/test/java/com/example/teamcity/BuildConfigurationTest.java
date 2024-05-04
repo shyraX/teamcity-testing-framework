0751373b9550b0f1ee805d6c422ec915f3955dd4
Alexander Kurov <a.kurov@petrel.ai>
2024-05-04 19:34:52 +0500

    Added negative tests for build configurations

--- src/test/java/com/example/teamcity/BuildConfigurationTest.java
+++ src/test/java/com/example/teamcity/BuildConfigurationTest.java
@@ -16,6 +16,9 @@ public class BuildConfigurationTest extends BaseApiTest {
     private static final String BUILD_CONFIG_STEP_TYPE = "simpleRunner";
     private static final String BUILD_CONFIG_PROPERTY_NAME = "script.content";
     private static final String BUILD_CONFIG_PROPERTY_VALUE = "echo 'Hello, World!'";
+    private static final String BUILD_CONFIG_WITH_SAME_ID_EXIST_ERROR = "The build configuration / template ID \"%s\" is already used by another configuration or template";
+    private static final String BUILD_CONFIG_WITH_EMPTY_ID_ERROR = "Build configuration or template ID must not be empty.";
+    private static final String BUILD_CONFIG_WITH_EMPTY_NAME_ERROR = "When creating a build type, non empty name should be provided.";
 
     @Test
     void creatingBuildConfigurationShouldBeAvailable() {
@@ -56,5 +59,46 @@ public class BuildConfigurationTest extends BaseApiTest {
                 .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
                 .body(Matchers.containsString(String.format(BUILD_CONFIG_WITH_SAME_NAME_EXIST_ERROR,
                         buildType.getName(), testData.getProject().getName())));
+    }
+
+    @Test
+    void creatingBuildConfigurationWithSameIdShouldNotBeAvailable() {
+
+        var testData = testDataStorage.addTestData();
+        var buildType = testData.getBuildType();
+        checkedWithSuperUser.getProjectRequest().create(testData.getProject());
+        checkedWithSuperUser.getBuildConfigRequest().create(buildType);
+
+        buildType.setName(RandomData.getString());
+        uncheckedWithSuperUser.getBuildConfigRequest().create(buildType)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString(String.format(BUILD_CONFIG_WITH_SAME_ID_EXIST_ERROR, buildType.getId())));
+    }
+
+    // 500 error?
+    @Test
+    void creatingBuildConfigurationWithEmptyIdShouldNotBeAvailable() {
+
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getProjectRequest().create(testData.getProject());
+        testData.getBuildType().setId("");
+
+        uncheckedWithSuperUser.getBuildConfigRequest().create(testData.getBuildType())
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString(BUILD_CONFIG_WITH_EMPTY_ID_ERROR));
+
+    }
+
+    @Test
+    void creatingBuildConfigurationWithEmptyNameShouldNotBeAvailable() {
+
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getProjectRequest().create(testData.getProject());
+        testData.getBuildType().setName("");
+
+        uncheckedWithSuperUser.getBuildConfigRequest().create(testData.getBuildType())
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString(BUILD_CONFIG_WITH_EMPTY_NAME_ERROR));
+
     }
 }
