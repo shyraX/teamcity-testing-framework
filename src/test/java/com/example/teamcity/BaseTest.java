2eea3cb9cdd8b1a12b00ae8e0e10e70d9aac855b
Alexander Kurov <a.kurov@petrel.ai>
2024-05-25 21:30:56 +0500

    moved setup and enableProjectPermissions methods to BaseApiTest

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,19 +1,12 @@
 package com.example.teamcity;
 
 import com.example.teamcity.api.generators.TestDataStorage;
-import com.example.teamcity.api.models.Module;
-import com.example.teamcity.api.models.Modules;
-import com.example.teamcity.api.models.ServerAuthSettings;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.requests.uncheked.UncheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
-import io.restassured.RestAssured;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-
-import java.util.Collections;
 
 public class BaseTest {
 
@@ -21,12 +14,6 @@ public class BaseTest {
     public TestDataStorage testDataStorage;
     public CheckedRequests checkedWithSuperUser = new CheckedRequests(Specifications.getSpec().superUserSpec());
     public UncheckedRequests uncheckedWithSuperUser = new UncheckedRequests(Specifications.getSpec().superUserSpec());
-    private static final String AUTH_SETTINGS_ENDPOINT = "/app/rest/server/authSettings";
-
-    @BeforeSuite
-    public void setup() {
-        enableProjectPermissions();
-    }
 
     @BeforeMethod
     public void setUpTest() {
@@ -46,21 +33,6 @@ public class BaseTest {
     @AfterMethod
     public void afterTest() {
         softy.assertAll();
-    }
-
-    private void enableProjectPermissions() {
-        var authSettings = ServerAuthSettings.builder()
-                .perProjectPermissions(true)
-                .modules(Modules.builder()
-                        .module(Collections.singletonList(Module.builder()
-                                .name("HTTP-Basic")
-                                .build()))
-                        .build())
-                .build();
-        RestAssured.given()
-                .spec(Specifications.getSpec().superUserSpec())
-                .body(authSettings)
-                .put(AUTH_SETTINGS_ENDPOINT);
     }
 
 }
--- src/test/java/com/example/teamcity/api/BaseApiTest.java
+++ src/test/java/com/example/teamcity/api/BaseApiTest.java
@@ -1,6 +1,36 @@
 package com.example.teamcity.api;
 
 import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.models.Module;
+import com.example.teamcity.api.models.Modules;
+import com.example.teamcity.api.models.ServerAuthSettings;
+import com.example.teamcity.api.spec.Specifications;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.Collections;
 
 public class BaseApiTest extends BaseTest {
+
+    private static final String AUTH_SETTINGS_ENDPOINT = "/app/rest/server/authSettings";
+
+    @BeforeSuite
+    public void setup() {
+        enableProjectPermissions();
+    }
+
+    private void enableProjectPermissions() {
+        var authSettings = ServerAuthSettings.builder()
+                .perProjectPermissions(true)
+                .modules(Modules.builder()
+                        .module(Collections.singletonList(Module.builder()
+                                .name("HTTP-Basic")
+                                .build()))
+                        .build())
+                .build();
+        RestAssured.given()
+                .spec(Specifications.getSpec().superUserSpec())
+                .body(authSettings)
+                .put(AUTH_SETTINGS_ENDPOINT);
+    }
 }
