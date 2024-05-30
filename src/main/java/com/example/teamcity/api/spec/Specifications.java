e32de261a0e7ace19042c39bbb020d593abc24de
Alexander Kurov <a.kurov@petrel.ai>
2024-05-30 22:18:46 +0500

    add filters and allure-rest-assured

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -2,12 +2,16 @@ package com.example.teamcity.api.spec;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
+import com.github.viclovsky.swagger.coverage.FileSystemOutputWriter;
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Specifications {
@@ -29,6 +33,9 @@ public class Specifications {
         var requestBuilder = new RequestSpecBuilder();
         requestBuilder.setBaseUri("http://" + Config.getProperty("host"));
         requestBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
+        requestBuilder.addFilter(new AllureRestAssured());
+        requestBuilder.addFilter(new SwaggerCoverageRestAssured(new FileSystemOutputWriter(
+                Paths.get("target/swagger-coverage-output"))));
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.setAccept(ContentType.JSON);
         return requestBuilder;
